1da65540dec18d5502595bae8bab0c71cc233c04
notebook40 <notebook40@163.com>
2019-09-13 23:11:12 +0800

    feat(common-pipeline): init compile step

--- src/com/notebook40/jenkins/pipeline/Pipelines.groovy
+++ src/com/notebook40/jenkins/pipeline/Pipelines.groovy
@@ -3,7 +3,7 @@ package com.notebook40.jenkins.pipeline
 import com.notebook40.jenkins.context.Context
 
 class Pipelines {
-  static def buildCommonPipeline(jenkins, pipelineParameters) {
+  static def buildCommonPipeline(jenkins, Map pipelineParameters) {
     Context context = new Context(jenkins, pipelineParameters)
 
     new DefaultPipelineBuilder().build(context)
--- src/com/notebook40/jenkins/step/CompileStep.groovy
+++ src/com/notebook40/jenkins/step/CompileStep.groovy
@@ -5,7 +5,7 @@ import com.notebook40.jenkins.context.Context
 class CompileStep extends MavenStep {
   @Override
   void process(Context context) {
-    echo(context, "maven image name: " + mavenImageName())
-    echo(context, "maven image args: " + mavenImageArgs())
+    echo(context, "maven image name: " + mavenImageName(context))
+    echo(context, "maven image args: " + mavenImageArgs(context))
   }
 }
