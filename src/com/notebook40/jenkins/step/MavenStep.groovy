4b088024a045649e6f5b751dd6c88766db86e913
notebook40 <notebook40@163.com>
2019-09-13 23:39:09 +0800

    feat(common-pipeline): init compile step

--- src/com/notebook40/jenkins/step/MavenStep.groovy
+++ src/com/notebook40/jenkins/step/MavenStep.groovy
@@ -3,6 +3,13 @@ package com.notebook40.jenkins.step
 import com.notebook40.jenkins.context.Context
 
 abstract class MavenStep extends AbstractStep {
+  def withMaven(Context context, Closure cl) {
+    context.jenkins.withDockerContainer(
+        "image": mavenImageName(context),
+        "args": mavenImageArgs(context)
+    )(cl)
+  }
+
   String mavenImageName(Context context) {
     return context.pipelineParameters["maven.image.name"]
   }
